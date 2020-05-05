12b53901336dbc3ca06b1efe511458e38aa5728e
Neal Conway <conwanea@gmail.com>
2020-05-04 19:24:47 -0500

    intial commit

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
